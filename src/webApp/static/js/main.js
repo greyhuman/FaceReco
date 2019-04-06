db3c3e42e024b4b2fee5863091c96d4908fa5d55
toalk <alexsandrtolich@yandex.ru>
2019-04-06 17:26:12 +0300

    changing titles for steps

--- src/webApp/static/js/main.js
+++ src/webApp/static/js/main.js
@@ -18,11 +18,11 @@ $(document).ready(function() {
                     success: function(data) {
                         if(data.imgs.length > 0){
                             $('#imgs-results').show();
-                            Results.putInto("results-source", 0, data.imgs[0], data, PutDataType.SIMPLE);
-                            Results.putInto("results-bb", 1, data.imgs[1], data, PutDataType.SIMPLE);
-                            Results.putInto("inner-result-kl", 2, data.imgs[2], data, PutDataType.KEYLABEL);
-                            Results.putInto("inner-result-rot", 3, data.imgs[3], data, PutDataType.ROTATION);
-                            Results.putInto("results-mark", 4, data.imgs[4], data, PutDataType.SIMPLE);
+                            Results.putInto("results-source", "The origin image:", 0, data.imgs[0], data, PutDataType.SIMPLE);
+                            Results.putInto("results-bb", 1, "The image with features and bounding boxes:", data.imgs[1], data, PutDataType.SIMPLE);
+                            Results.putInto("inner-result-kl", "The found faces:", 2, data.imgs[2], data, PutDataType.KEYLABEL);
+                            Results.putInto("inner-result-rot", "The aligned faces:", 3, data.imgs[3], data, PutDataType.ROTATION);
+                            Results.putInto("results-mark", 4, "The stub:", data.imgs[4], data, PutDataType.SIMPLE);
 
                             var metricPerform = data.metrics.time;
                             metricPerform = Math.round(metricPerform * 10000) / 10000;
@@ -58,14 +58,8 @@ $(document).ready(function() {
     PutDataType = {SIMPLE: 1, KEYLABEL: 2, ROTATION: 3}
 
     Results = {
-        putInto  : function(tegId, stepNumber, pathToMainImage, pathToRoot, type){ // type = simple, keylabel, rotation
-            var title = '';
+        putInto  : function(tegId, title, stepNumber, pathToMainImage, pathToRoot, type){ // type = simple, keylabel, rotation
             var elementHtml = '';
-            if(stepNumber == 0){
-                title = 'The origin image:';
-            } else {
-                title = 'The step ' + stepNumber + ':';
-            }
             elementHtml += '<p class="lead">' + title + '</p>';
             if(type == PutDataType.SIMPLE){
                 elementHtml += Results.getImgItem(pathToMainImage, '', '');
