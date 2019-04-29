79fb8345da845af288a95e4b5ffca501f1964b82
zofinka <sofyabalandina1596@gmail.com>
2019-04-29 22:14:32 +0300

    Update scripts for install and install check

--- src/install_check.sh
+++ src/install_check.sh
@@ -11,9 +11,9 @@ do
 	echo -n "check" $MODULE  "installed..."
 	if [ $(dpkg-query -W -f='${Status}' $MODULE 2>/dev/null | grep -c "ok installed") -eq 0 ];
 	then
-   		echo -e "no"
+   		echo -e "$(tput setaf 1)no$(tput sgr 0)"
 	else
-		echo -e "yes"
+		echo -e "$(tput setaf 2)yes$(tput sgr 0)"
 	fi
 done
 
@@ -22,24 +22,27 @@ echo -e
 echo -e
 
 
-PYTHON_MODULES=('matplotlib' 'cv2' 'dlib' 'openface' 'face_recognition_models' 'flask' 'numpy')
+PYTHON_MODULES=('matplotlib' 'cv2' 'dlib' 'openface' 'face_recognition_models' 'flask' 'numpy' 'keras' 'tensorflow')
 
 
 for MODULE in ${PYTHON_MODULES[@]}
 do
 	echo -n "check" $MODULE  "installed..."
 	COMMAND=""
-	if [ $MODULE == 'openface' ]
+	if [ $MODULE == 'openface' ];
 	then
 		COMMAND="import $MODULE;"
+	elif [ $MODULE == 'cv2' ]; 
+	then
+		COMMAND="import $MODULE; print($MODULE.__version__); print(help($MODULE.dnn));"
 	else
 		COMMAND="import $MODULE; print($MODULE.__version__)"
 	fi
 	if [ $(python -c "$COMMAND" 2>/dev/null |& grep -c "Error:") -eq 0 ];
 	then
-   		echo -e "yes"
+   		echo -e "$(tput setaf 2)yes$(tput sgr 0)"
 	else
-		echo -e "no"
+		echo -e "$(tput setaf 1)no$(tput sgr 0)"
 	fi
 done
 
